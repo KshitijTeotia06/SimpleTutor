5298e2a4bc025f85f488f0c5f83649f9ee2de793
Kshitij Teotia <teotia.kshitij@gmail.com>
2022-06-10 00:53:48 -0700

    added changes

--- src/components/sections/FeaturesSplit.js
+++ src/components/sections/FeaturesSplit.js
@@ -172,7 +172,7 @@ const FeaturesSplit = ({
             <div className="split-item">
               <div className="split-item-content center-content-mobile reveal-from-right" data-reveal-container=".split-item">
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
-                  Website Developer
+                  Head of Software Development
                   </div>
                 <h3 className="mt-0 mb-12">
                   Kshitij Teotia
