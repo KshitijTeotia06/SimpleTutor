086949e3795c349ac4075f6461339c7c75bdbf56
Kshitij Teotia <teotia.kshitij@gmail.com>
2022-06-10 00:14:15 -0700

    Updates

---
+++ precache-manifest.f86d23944fb55b8f8ba1fff0171bbc3e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee25da2367d652925587ab2e317cecf0",
+    "url": "/SimpleTutor/index.html"
+  },
+  {
+    "revision": "beb51a830cc2ca857094",
+    "url": "/SimpleTutor/static/css/main.d6edcffa.chunk.css"
+  },
+  {
+    "revision": "af68cf42ccc525ccdcd0",
+    "url": "/SimpleTutor/static/js/2.587d06b7.chunk.js"
+  },
+  {
+    "revision": "27fa203eaf83874a090f4b81835b0502",
+    "url": "/SimpleTutor/static/js/2.587d06b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beb51a830cc2ca857094",
+    "url": "/SimpleTutor/static/js/main.8f7e4cad.chunk.js"
+  },
+  {
+    "revision": "3a2bdb5ec7b42a34a0e1",
+    "url": "/SimpleTutor/static/js/runtime-main.0f3538c9.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/SimpleTutor/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/SimpleTutor/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/SimpleTutor/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/SimpleTutor/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/SimpleTutor/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/SimpleTutor/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/SimpleTutor/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/SimpleTutor/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/SimpleTutor/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/SimpleTutor/static/media/logo.2810a88b.svg"
+  }
+]);
